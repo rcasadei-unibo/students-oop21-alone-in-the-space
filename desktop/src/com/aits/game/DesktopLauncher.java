6db71d496cc85d5c559a88ca10d5d386a03ca7a7
Daniele Martignani <daniele.martignani@studio.unibo.it>
2022-08-08 17:03:09 +0200

    first commit

---
+++ desktop/src/com/aits/game/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.aits.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+import controller.AloneInTheSpace;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Alone in the space");
+		new Lwjgl3Application(new AloneInTheSpace(), config);
+	}
+}
