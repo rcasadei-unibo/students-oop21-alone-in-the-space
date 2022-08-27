052941f6490eed143990fa55ba546accbe094961
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-08-27 13:14:42 +0200

    add GameEngineImpl

---
+++ core/src/model/GameEngineImpl.java
@@ -0,0 +1,35 @@
+package model;
+
+public class GameEngineImpl implements GameEngine {
+
+	@Override
+	public void mainLoop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void processInput() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(int elapsed) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initGame() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
