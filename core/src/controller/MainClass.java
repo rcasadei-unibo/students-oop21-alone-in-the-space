2c3c0d33d83cb33887d1152ca06ed40657115a65
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-09-23 17:21:54 +0200

    add class launcher

---
+++ core/src/controller/MainClass.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.application.Application;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Application.launch(Testing.class, args);
+	}
+
+}
