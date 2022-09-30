5551d1e923bf61f2fd56ffde768388b84c80f90a
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-09-30 09:30:27 +0200

    add doc

--- core/src/controller/Collision.java
+++ core/src/controller/Collision.java
@@ -1,12 +1,22 @@
 package controller;
 
 public interface Collision {
-
 	
+	/**
+	 * Check if there is a collision with enemies
+	 * 
+	 * @return True if a collision occurred
+	 */
 	boolean checkEnemiesCollision();
-	
+	/**
+	 * Check if there is a collision with bullets
+	 * 
+	 * @return True if a collision occurred
+	 */
 	boolean checkBulletsCollision();
-	
+	/**
+	 * Check if there is a collision with borders
+	 */
 	void checkBorderCollision();
 	
 }
