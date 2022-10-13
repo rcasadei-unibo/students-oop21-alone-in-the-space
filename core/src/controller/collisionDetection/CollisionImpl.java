8155a0239d580b9b2fce310108c28ab07b4028e1
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-10-13 22:15:25 +0200

    refactoring

--- core/src/controller/collisionDetection/CollisionImpl.java
+++ core/src/controller/collisionDetection/CollisionImpl.java
@@ -1,26 +1,17 @@
 package controller.collisionDetection;
 
-import javafx.scene.shape.Rectangle;
 import model.Entity;
 
 public class CollisionImpl implements Collision {
 
 	@Override
 	public boolean checkEnemiesCollision(Entity ship, Entity enemy) {
-		if(ship.getNode().getBoundsInParent().intersects(enemy.getNode().getBoundsInParent())) {
-			System.out.println("Collision");
-			return true;
-		}
-		return false;
+		return ship.getNode().getBoundsInParent().intersects(enemy.getNode().getBoundsInParent());
 	}
 
 	@Override
 	public boolean checkBulletsCollision(Entity ship, Entity bullet) {
-		if(ship.getNode().getBoundsInParent().intersects(bullet.getNode().getBoundsInParent())) {
-			System.out.println("Collision");
-			return true;
-		}
-		return false;
+		return ship.getNode().getBoundsInParent().intersects(bullet.getNode().getBoundsInParent());
 	}
 
 	@Override
