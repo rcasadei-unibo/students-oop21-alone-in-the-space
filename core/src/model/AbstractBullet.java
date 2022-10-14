6a0377f38cc475b5f566538afcab9f8b6cc29980
giuseppe pintus <giuseppe.pintus2@studio.unibo.it>
2022-10-14 18:50:51 +0200

    fixed

--- core/src/model/AbstractBullet.java
+++ core/src/model/AbstractBullet.java
@@ -65,6 +65,12 @@ public abstract class AbstractBullet implements Bullet {
 		// TODO Auto-generated method stub
 		return this.direction.cpy();
 	}
+	@Override
+	public void setPosition(Vector2 newpos) {
+		// TODO Auto-generated method stub
+		 this.position=newpos;
+		 return ;
+	}
 	// TODO Auto-generated method stub
 		public void setSprite(Image img) {
 			this.sprite=new ImageView();
