4214bfeb6f9c9d1c46322ee26e9357abb106c397
giuseppe pintus <giuseppe.pintus2@studio.unibo.it>
2022-10-16 15:23:55 +0200

    added docs for interface

--- core/src/model/Bullet.java
+++ core/src/model/Bullet.java
@@ -1,12 +1,34 @@
 package model;
 
+import java.io.IOException;
+
 import com.badlogic.gdx.math.Vector2;
 
 public interface Bullet extends Entity{
 	//public void move(float deltaTime);
+	
+	/**
+     *	statement if this bullet actually has collided.
+     */
 	public Boolean isCollided();
+	
+	/**
+     *	destroy this bullet.
+     */
 	public void destroy();
+	
+	/**
+     *	the actual damage this bullet provide on hit.
+     */
 	public float getDamage();
+	
+	/**
+     *	return the position of this bullet.
+     */
 	public Vector2 getPosition();
+	
+	/**
+     *	return the direction of this bullet.
+     */
 	public Vector2 getDirection();
 }
--- core/src/model/Entity.java
+++ core/src/model/Entity.java
@@ -5,24 +5,58 @@ import javafx.scene.Node;
 import javafx.scene.image.Image;
 
 import java.io.File;
+import java.io.IOException;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
 public interface Entity {
-
+	
+	 /**
+     * Get the position of the entity
+     *
+     */
 	Vector2 getPosition();
+	
+	 /**
+     * Get the direction of the entity
+     *
+     */
 	Vector2 getDirection();
 	
-	
+	 /**
+     * boolean statement if the entity is still alive
+     *
+     */
 	Boolean isAlive();
-
+	
+	 /**
+     * destroy all item attached to the object
+     *
+     */
 	void destroy();
 
+	 /**
+     * move the entity for the interval specified
+     *
+     */
 	void move(float deltaTime);
 
+	 /**
+     * return the node (of javaFX) associate at the object
+     *
+     */
 	Node getNode();
 
-	void setSprite(Image img) ;
+	 /**
+     *	Set the sprite of the entity
+     *
+     */
+	void setSprite(Image img);
+	
+	 /**
+     * Set the position.
+     *
+     */
 	void setPosition(Vector2 newpos);
 }
