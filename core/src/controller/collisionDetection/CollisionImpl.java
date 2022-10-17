36311e3a901052d2bee850e68b56233b99f75883
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2022-10-17 19:52:18 +0200

    add  checkAllCollision

--- core/src/controller/collisionDetection/CollisionImpl.java
+++ core/src/controller/collisionDetection/CollisionImpl.java
@@ -1,22 +1,40 @@
 package controller.collisionDetection;
 
+import java.util.Collection;
+
 import model.Entity;
 
 public class CollisionImpl implements Collision {
 
 	@Override
-	public boolean checkEnemiesCollision(Entity ship, Entity enemy) {
+	public boolean checkEnemyCollision(Entity ship, Entity enemy) {
 		return ship.getNode().getBoundsInParent().intersects(enemy.getNode().getBoundsInParent());
 	}
 
 	@Override
-	public boolean checkBulletsCollision(Entity ship, Entity bullet) {
+	public boolean checkBulletCollision(Entity ship, Entity bullet) {
 		return ship.getNode().getBoundsInParent().intersects(bullet.getNode().getBoundsInParent());
 	}
 
 	@Override
 	public void checkBorderCollision(Entity ship) {
 		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void checkAllCollision(Entity ship, Collection<Entity> enemies, Collection<Entity> bullets) {
+		enemies.forEach((Entity enemy) -> {
+			if(checkEnemyCollision(ship, enemy)) {
+				//TO DEVELOP
+			}
+		});
+		
+		bullets.forEach((Entity bullet) -> {
+			if(checkBulletCollision(ship, bullet)) {
+				//TO DEVELOP
+			}
+		});
 
 	}
 
